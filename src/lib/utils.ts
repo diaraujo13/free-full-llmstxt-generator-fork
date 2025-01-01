f83f66cd1891044f1d925c2258a2340ec19215f7
Moinul Moin <moinulmoin101@gmail.com>
2025-01-02 01:11:12 +0600

    fix: :bug: correct siteUrl assignment to ensure proper URL handling based on environment variable

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,4 +50,4 @@ export function extractContent($: CheerioAPI) {
   };
 }
 
-export const siteUrl = `https://${process.env.VERCEL_URL}` || "http://localhost:3000";
+export const siteUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
